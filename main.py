aac4d179cf7ffba21adc7e3723bb48c6ef62f100
Aleksandr <popkov.aleks.v@yandex.ru>
2022-11-09 10:43:15 +0500

    corrections

--- main.py
+++ main.py
@@ -4,38 +4,38 @@ import time
 
 from environs import Env
 
-env = Env()
-env.read_env()
-
-
-TELEGRAM_TOKEN = env('TELEGRAM_TOKEN')
-LONG_POLLING_URL = env('LONG_POLLING_URL')
-AUTHORIZATION_TOKEN = env('AUTHORIZATION_TOKEN')
-CHAT_ID = env('CHAT_ID')
-
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = None
-    
+
     while True:
         try:
             payload = {'timestamp': timestamp}
             response = requests.get(f'{LONG_POLLING_URL}',
                                     params=payload,
-                                    headers={'Authorization': f'Token {AUTHORIZATION_TOKEN}'}, timeout=90)
-            timestamp = response.json().get('last_attempt_timestamp')
-            
+                                    headers={
+                                        'Authorization': f'Token \
+                                            {AUTHORIZATION_TOKEN}'
+                                        },
+                                    timeout=90)
+
+            resp_json = response.json()
+            timestamp = resp_json.get('last_attempt_timestamp')
+
             if response.status_code != 200:
                 print(f'status: {response.status_code}, text: {response.text}')
-            
-            if response.json().get('status') == 'found':
-                for attempt in response.json().get('new_attempts'):
-                    mistake = 'Есть замечания!' if attempt.get('is_negative') else ''
-                    message = f'''Преподаватель проверил работу {attempt.get('lesson_title')}!
+
+            if resp_json.get('status') == 'found':
+                for attempt in resp_json.get('new_attempts'):
+                    mistake = 'Есть замечания!' if attempt.get('is_negative') \
+                        else ''
+                    message = f'''Преподаватель проверил работу \
+                        {attempt.get('lesson_title')}!
                     {mistake}
                     Ссылка на задание: {attempt.get('lesson_url')}'''
-                    bot.send_message(text=message, chat_id=CHAT_ID)
+
+                    bot.send_message(text=message, chat_id=TG_CHAT_ID)
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
@@ -43,4 +43,12 @@ def main():
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    TELEGRAM_TOKEN = env('TELEGRAM_TOKEN')
+    LONG_POLLING_URL = env('LONG_POLLING_URL')
+    AUTHORIZATION_TOKEN = env('AUTHORIZATION_TOKEN')
+    TG_CHAT_ID = env('TG_CHAT_ID')
+
     main()
