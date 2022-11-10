00b8f5283b47d8197894cb4376cc68359af4ac0e
Aleksandr <popkov.aleks.v@yandex.ru>
2022-11-10 12:06:07 +0500

    variables name

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ def main():
     while True:
         try:
             payload = {'timestamp': timestamp}
-            resp_orig = requests.get(f'{LONG_POLLING_URL}',
-                                     params=payload,
-                                     headers={
+            response = requests.get(f'{LONG_POLLING_URL}',
+                                    params=payload,
+                                    headers={
                                         'Authorization': f'Token \
                                             {AUTHORIZATION_TOKEN}'
                                         },
-                                     timeout=90)
+                                    timeout=90)
 
-            response = resp_orig.json()
-            timestamp = response.get('last_attempt_timestamp')
+            homeworks = response.json()
+            timestamp = homeworks.get('last_attempt_timestamp')
 
-            if resp_orig.status_code != 200:
-                print(f'status: {resp_orig.status_code},\
-                      text: {resp_orig.text}')
+            if response.status_code != 200:
+                print(f'status: {response.status_code},\
+                      text: {response.text}')
 
-            if response.get('status') == 'found':
-                for attempt in response.get('new_attempts'):
+            if homeworks.get('status') == 'found':
+                for attempt in homeworks.get('new_attempts'):
                     mistake = 'Есть замечания!' if attempt.get('is_negative') \
                         else ''
                     message = f'''Преподаватель проверил работу \
